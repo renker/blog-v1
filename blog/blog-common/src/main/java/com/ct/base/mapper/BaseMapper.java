33bd08bc1ee3a654bf3efd3d1b162e29aba7507a
renker <caishenchen@163.com>
2016-04-23 14:37:13 +0800

    1

---
+++ blog/blog-common/src/main/java/com/ct/base/mapper/BaseMapper.java
@@ -0,0 +1,8 @@
+package com.ct.base.mapper;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BaseMapper {
+
+}
--- blog/blog-service/src/main/java/com/ct/person/dao/PersonMapper.java
+++ blog/blog-service/src/main/java/com/ct/person/dao/PersonMapper.java
@@ -1,11 +1,19 @@
 package com.ct.person.dao;
 
+
+import com.ct.base.mapper.BaseMapper;
 import com.ct.person.entity.Person;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface PersonMapper {
+
+public interface PersonMapper extends BaseMapper{
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Person record);
 
     int insertSelective(Person record);
 
+    Person selectByPrimaryKey(Long id);
+
     int updateByPrimaryKeySelective(Person record);
+
+    int updateByPrimaryKey(Person record);
 }
